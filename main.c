02b6fb94471bc32426bc4d4283262e1346a2fd65
jsolosky735 <jjs7331@psu.edu>
2020-09-03 18:43:08 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+//Author: Jamys Solosky jjs7331@psu.edu 
+//Collaborator: Timothy Zheng txz5165@psu.edu 
+//Collaborator: Colton Giordano cjg5896@psu.edu
+//Collaborator: Lauren Hughes lmh5981@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char*temp = readline("Enter temperature celsius: ");
+  double celsius = atof(temp);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit. \n", celsius, (celsius*(9/5))+32);
+}
